b2d8e74102b54c231f93942618f0f26e496bf484
Hemnys <hemnys25@gmail.com>
2021-02-07 17:16:05 -0500

    format code

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const stringToHTML = (s) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(s, "text/html");
-    return doc.body.firstChild;
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(s, "text/html");
+  return doc.body.firstChild;
 };
 const addEvents = (selector, id) => {
-    document.querySelector('body').appendChild(selector);
-    window.onclick = function (event) {
-        if (event.target === selector || event.target.id === `close-${id}`) {
-            selector.querySelector('.modal-content').style.animationName = 'animatetopOut'
-            setTimeout(() => { document.querySelector('body').removeChild(selector); }, 200);
-        }
+  document.querySelector("body").appendChild(selector);
+  window.onclick = function (event) {
+    if (event.target === selector || event.target.id === `close-${id}`) {
+      selector.querySelector(".modal-content").style.animationName =
+        "animatetopOut";
+      setTimeout(() => {
+        document.querySelector("body").removeChild(selector);
+      }, 200);
     }
-}
+  };
+};
 const showModal = (character) => {
-    const { name, id, image, status, species, gender, origin } = character;
-    let template = `
+  const { name, id, image, status, species, gender, origin } = character;
+  let template = `
     <div id="myModal-${id}" class="modal">
       <div class="modal-content">
         <span class="close" id="close-${id}">&times;</span>
@@ -33,12 +36,12 @@ const showModal = (character) => {
       </div>
     </div>
     `;
-    renderedModal = stringToHTML(template);
-    addEvents(renderedModal, id)
-}
+  renderedModal = stringToHTML(template);
+  addEvents(renderedModal, id);
+};
 const showCard = (character) => {
-    const { name, id, image } = character;
-    let template = `
+  const { name, id, image } = character;
+  let template = `
         <div class="character fade-in" data-id="${id}">
             <div class="character-image-wrapper">
                 <img src="${image}" alt="${name}">
@@ -48,37 +51,38 @@ const showCard = (character) => {
             </div>
         </div>
     `;
-    renderedCharacter = stringToHTML(template);
-    renderedCharacter.addEventListener('click', function () {
-        showModal(character);
-    })
-    return renderedCharacter;
-}
-const getData = URL => {
-    const loading = document.querySelector('.loading');
-    loading.style.display = 'block';
-    fetch(URL)
-        .then(res => res.json())
-        .then(characters => {
-            loading.style.display = 'none';
-            if (characters.info.next) {
-                document.getElementById('load-more').setAttribute('data-next-url', characters.info.next);
-                document.getElementById('load-more').removeAttribute('disabled')
-            }
-            characters.results.map(character => {
-                const charactersList = document.querySelector('.characters-wrapper')
-                let renderedCharacters = showCard(character);
-                charactersList.appendChild(renderedCharacters);
-
-            })
-        })
-}
+  renderedCharacter = stringToHTML(template);
+  renderedCharacter.addEventListener("click", function () {
+    showModal(character);
+  });
+  return renderedCharacter;
+};
+const getData = (URL) => {
+  const loading = document.querySelector(".loading");
+  loading.style.display = "block";
+  fetch(URL)
+    .then((res) => res.json())
+    .then((characters) => {
+      loading.style.display = "none";
+      if (characters.info.next) {
+        document
+          .getElementById("load-more")
+          .setAttribute("data-next-url", characters.info.next);
+        document.getElementById("load-more").removeAttribute("disabled");
+      }
+      characters.results.map((character) => {
+        const charactersList = document.querySelector(".characters-wrapper");
+        let renderedCharacters = showCard(character);
+        charactersList.appendChild(renderedCharacters);
+      });
+    });
+};
 window.onload = () => {
-    const URL = 'https://rickandmortyapi.com/api/character/';
+  const URL = "https://rickandmortyapi.com/api/character/";
+  getData(URL);
+  document.getElementById("load-more").addEventListener("click", function () {
+    let URL = this.dataset.nextUrl;
     getData(URL);
-    document.getElementById('load-more').addEventListener('click', function () {
-        let URL = this.dataset.nextUrl;
-        getData(URL);
-        this.setAttribute('disabled', 'disabled');
-    })
-}
+    this.setAttribute("disabled", "disabled");
+  });
+};
